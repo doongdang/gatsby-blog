24ef54e1e1cc4748a401b2f2c53e2f08aeb7dfc5
SeokHyun Cho <doongdang@naver.com>
2020-05-05 22:32:57 +0900

    Create & Fetching Post With Markdown

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,51 @@
 import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import { graphql, StaticQuery } from "gatsby"
+import Post from "../components/Post"
 
 const IndexPage = () => (
   <Layout>
     <SEO title={"Home"} keywords={[`gatsby`, `application`, `react`]} />
     <h1>Home Page</h1>
+    <StaticQuery
+      query={indexQuery}
+      render={data => {
+        return (
+          <div>
+            {data.allMarkdownRemark.edges.map(({ node }) => (
+              <Post
+                key={node.id}
+                title={node.frontmatter.title}
+                author={node.frontmatter.author}
+                path={node.frontmatter.path}
+                date={node.frontmatter.date}
+                body={node.excerpt}
+              />
+            ))}
+          </div>
+        )
+      }}
+    />
   </Layout>
 )
+const indexQuery = graphql`
+  query MyQuery {
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+      edges {
+        node {
+          id
+          frontmatter {
+            date(formatString: "MMM Do YYYY")
+            author
+            path
+            title
+          }
+          excerpt
+        }
+      }
+    }
+  }
+`
 
 export default IndexPage
---
+++ src/pages/posts/003-third-post.md
@@ -0,0 +1,8 @@
+---
+title: "Third Post - My Third Post"
+date: 2020-05-05 20:22:00
+author: "SeokHyun Cho"
+path: "/third-post"
+---
+
+어렵다 어려워. 어렵다 어려워. 어렵다 어려워. 어렵다 어려워. 어렵다 어려워. 어렵다 어려워. 어렵다 어려워.
